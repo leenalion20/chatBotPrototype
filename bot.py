e9a01c641330fd444506441529eb746bc50ad940
leenalion20 <samissua@espol.edu.ec>
2022-04-06 18:13:26 -0500

    Modificaciones menores

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
-import openai
+import openai as ai
 
-openai.api_key = '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'
+#Llave de acceso a la API de OpenAI
+ai.api_key = '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'
 
 app = Flask(__name__)
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    incoming_msg = request.values.get('Body', '').lower()
-    resp = MessagingResponse()
-    msg = resp.message()
+    mensaje_recibido = request.values.get('Body', '').lower()
+    respuesta = MessagingResponse()
+    mensaje = respuesta.message()
     
-    ai=openai.Completion.create(
+    robot=ai.Completion.create(
         engine='davinci', 
-        prompt="\nHumano: "+incoming_msg+"\nAI:",
+        prompt="\nHumano: "+mensaje_recibido+"\nAI:",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -25,9 +26,9 @@ def bot():
         stop=["\n","Humano:", "AI: "]
         )
     
-    msg.body(ai.choices[0].text.strip())
+    mensaje.body(robot.choices[0].text.strip())
     
-    return str(resp)
+    return str(respuesta)
 
 
 if __name__ == '__main__':
